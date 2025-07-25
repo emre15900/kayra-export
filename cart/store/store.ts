6609d23bd439123755b0e9481fd42a76177ee50b
Emre Sayın <emresayinweb@gmail.com>
2025-07-25 03:17:22 +0300

    update

--- cart/store/store.ts
+++ cart/store/store.ts
@@ -1,11 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './cartSlice'
 
-export const store = configureStore({
+let store: any
+
+const createStore = () => configureStore({
   reducer: {
     cart: cartReducer,
   },
 })
 
+if (typeof window === 'undefined') {
+  store = createStore()
+} else {
+  // Create store if unavailable on client side
+  if (!store) {
+    store = createStore()
+  }
+}
+
+export { store }
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
--- home/store/store.ts
+++ home/store/store.ts
@@ -2,12 +2,24 @@ import { configureStore } from '@reduxjs/toolkit'
 import productReducer from './productSlice'
 import cartReducer from './cartSlice'
 
-export const store = configureStore({
+let store: any
+
+const createStore = () => configureStore({
   reducer: {
     products: productReducer,
     cart: cartReducer,
   },
 })
 
+if (typeof window === 'undefined') {
+  store = createStore()
+} else {
+  // Create store if unavailable on client side
+  if (!store) {
+    store = createStore()
+  }
+}
+
+export { store }
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
