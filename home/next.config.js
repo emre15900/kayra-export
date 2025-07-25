eabfba0c849264feb997c120a084b165a5331541
Emre Sayın <emresayinweb@gmail.com>
2025-07-25 03:35:53 +0300

    update configs

--- home/next.config.js
+++ home/next.config.js
@@ -1,26 +1,40 @@
-const NextFederationPlugin = require('@module-federation/nextjs-mf');
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config, options) {
-    const { isServer } = options;
-    
     config.plugins.push(
       new NextFederationPlugin({
-        name: 'home',
-        filename: 'static/chunks/remoteEntry.js',
+        name: "home",
+        filename: "static/chunks/remoteEntry.js",
         exposes: {
-          './ProductCard': './components/ProductCard',
-          './store': './store/store',
+          "./ProductCard": "./components/ProductCard",
+          "./store": "./store/store",
         },
         remotes: {
-          cart: `cart@http://localhost:3001/_next/static/chunks/remoteEntry.js`,
+          cart: "cart@http://localhost:3001/_next/static/chunks/remoteEntry.js", // cart remote'u tanımlanıyor
         },
         shared: {
-          react: { singleton: true, eager: true },
-          'react-dom': { singleton: true, eager: true },
-          '@reduxjs/toolkit': { singleton: true, eager: true },
-          'react-redux': { singleton: true, eager: true },
+          react: {
+            singleton: true,
+            requiredVersion: false,
+            eager: true,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: false,
+            eager: true,
+          },
+          "react-redux": {
+            singleton: true,
+            requiredVersion: false,
+            eager: true,
+          },
+          "@reduxjs/toolkit": {
+            singleton: true,
+            requiredVersion: false,
+            eager: true,
+          },
         },
         extraOptions: {
           exposePages: true,
